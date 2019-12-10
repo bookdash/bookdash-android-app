e4d0ad5940cce4960f2a071127573d6aa0733f30
HeinrichWesson <heinrich@thereachtrust.org>
2019-12-10 09:38:19 +0200

    ADD Material Intro Tutorial library as a module and migrate it to Androidx and Glide V4. The library was is deprecated so maintenance will now be within this project.

---
+++ materialhelptutorial/src/main/java/za/co/riggaroo/materialhelptutorial/tutorial/MaterialTutorialContract.java
@@ -0,0 +1,54 @@
+/*
+MIT License
+
+Copyright (c) 2015 Rebecca Franks
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+and associated documentation files (the "Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject
+to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or
+substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+package za.co.riggaroo.materialhelptutorial.tutorial;
+
+import java.util.List;
+
+import za.co.riggaroo.materialhelptutorial.MaterialTutorialFragment;
+import za.co.riggaroo.materialhelptutorial.TutorialItem;
+
+/**
+ * @author rebeccafranks
+ * @since 15/11/09.
+ */
+public interface MaterialTutorialContract {
+
+    interface View {
+        void showNextTutorial();
+        void showEndTutorial();
+        void setBackgroundColor(int color);
+        void showDoneButton();
+        void showSkipButton();
+        void setViewPagerFragments(List<MaterialTutorialFragment> materialTutorialFragments);
+    }
+
+    interface UserActionsListener {
+        void loadViewPagerFragments(List<TutorialItem> tutorialItems);
+        void doneOrSkipClick();
+        void nextClick();
+        void onPageSelected(int pageNo);
+        void transformPage(android.view.View page, float position);
+
+        int getNumberOfTutorials();
+    }
+
+}
